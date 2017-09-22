329ec5cbd3c2edda69ac3d5a0330e473a3270268
darryl west <darryl.west@RainCitySoftware.com>
2017-09-22 12:08:42 -0700

    added maven to the install list

---
+++ install-scripts/install-maven.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# dpw@alameda.local
+# 2017.09.22
+#
+
+VERS=3.5.0
+DIST="apache-maven-$VERS"
+TAR="$DIST-bin.tar.gz"
+
+cd /opt \
+    && wget "http://www-eu.apache.org/dist/maven/maven-3/$VERS/binaries/$TAR" \
+    && tar xvzf $TAR \
+    && ln -s $PWD/$DIST/bin/mvn /usr/local/bin/mvn
+
+exit 0
