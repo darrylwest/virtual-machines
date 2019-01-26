2f7f6eab5a40383ae58a5f4daf2ec59523062d8f
darryl west <darryl.west@RainCitySoftware.com>
2019-01-26 06:32:45 -0800

    updated elixir installation script

--- debian-9/install-scripts/install-elixir.sh
+++ debian-9/install-scripts/install-elixir.sh
@@ -7,5 +7,6 @@
     && dpkg -i erlang-solutions_1.0_all.deb \
     && apt-get update -y \
     && apt-get install esl-erlang -y \
-    && apt-get install elixir -y
+    && apt-get install elixir -y \
+    && mix local.hex
 
