5d74e50fdb2206b29ea0a9bb501aea29ed282597
darryl west <darryl.west@RainCitySoftware.com>
2019-02-13 14:11:38 -0800

    added go install script and ignore for cover

---
+++ debian-9/install-scripts/install-golang.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# dpw@seattle.local
+# 2017.03.05
+#
+
+set -eu
+
+installGolang() {
+    [ -d /downloads ] || mkdir /downloads
+    cd /downloads || {
+        echo "error! no downloads folder! exiting..."
+        exit 1
+    }
+
+    vers=1.11.5
+    wget "https://dl.google.com/go/go${vers}.linux-amd64.tar.gz"
+    cd /usr/local
+
+    tar -xvzf "/downloads/go${vers}.linux-amd64.tar.gz" \
+        && ln -s /usr/local/go/bin/go /usr/local/bin/go \
+        && ln -s /usr/local/go/bin/godoc /usr/local/bin/godoc \
+        && ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt 
+
+}
+
+installGolang
+
