678a1f2b29ab8d96bca32fa508c762a063e9584c
darryl west <darryl.west@RainCitySoftware.com>
2019-03-05 05:55:00 -0800

    removed postgresql and mongoose

--- debian-9/install-scripts/post-install.sh
+++ debian-9/install-scripts/post-install.sh
@@ -4,16 +4,12 @@
 #
 
 sudo cp /vagrant/install-scripts/motd /etc/motd
-sudo systemctl start postgresql.service
 
 /vagrant/install-scripts/keygen-linux
 /vagrant/install-scripts/install-phoenix.sh
 cp /vagrant/install-scripts/gitconfig .gitconfig
 
-[ -d MongooseIM ] || git clone https://github.com/esl/MongooseIM.git
 [ -d ex_twillo ] || git clone https://github.com/danielberkompas/ex_twilio.git
-[ -d cuid ] || git clone https://github.com/duailibe/cuid.git
-[ -d cloak] || git clone https://github.com/danielberkompas/cloak
 
 # copy the id_rsa first...
 [ -d skedgealert ] || git clone git@gitlab.com:SkedgeAlert/skedgealert.git
